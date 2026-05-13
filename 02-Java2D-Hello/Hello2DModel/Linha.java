84f615c418ca673c3df11cbbf9d0e8b2f4064281
luizfelipe3675 <lufelipegarcia290507@gmail.com>
2026-05-13 17:42:16 -0300

    Update Linha.java

--- 02-Java2D-Hello/Hello2DModel/Linha.java
+++ 02-Java2D-Hello/Hello2DModel/Linha.java
@@ -1,1 +1,32 @@
+package projetoLP2.model;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+public class Linha implements Figura{
+	private int xi,yi,xf,yf,e,r,g,b;
+	public Linha(int xi,int yi,int xf,int yf,int e,int r,int g,int b){
+		this.xi=xi;
+		this.yi=yi;
+		this.xf=xf;
+		this.yf=yf;
+		this.e=e;
+		this.r=r;
+		this.g=g;
+		this.b=b;
+
+		if(r<0||r>255||g<0||g>255||b<0||b>255){
+			throw new IllegalArgumentException("cor invalida");	
+		}
+	}
+	@Override
+	public void desenhar(Graphics2D g2d){
+		AffineTransform old =g2d.getTransform();
+		g2d.setColor(new Color(r,g,b));
+		g2d.setStroke(new BasicStroke(e));
+		g2d.drawLine(xi,yi,xf,yf);
+		g2d.setTransform(old);
+	}
+	public void print(){
+		System.out.printf("Linha com espessura(%d) com cor RGB(%d,%d,%d) com ponto inicial(%d,%d) e ponto final(%d,%d) \n",e,r,g,b,xi,yi,xf,yf);
+	}
+}
 
